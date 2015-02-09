3fc8a88ec3eedefbd4eb4b1d987e2f50ffb2ec47
jonabasque <jonmultimedia@gmail.com>
2015-02-09 18:13:27 +0100

    Agregada vista del portfolio gamaker a layout portfolio del theme

---
+++ wp-content/themes/accesspress-parallax-hijo/layouts/portfolio-section.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying all Parallax Templates.
+ *
+ * @package accesspress_parallax
+ */
+?>
+
+	<div class="portfolio-listing clearfix">
+		<?php echo do_shortcode("[huge_it_portfolio id='1']"); ?>
+	</div><!-- #primary -->
